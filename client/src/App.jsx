9048742c1bbb0bbbd5798a8ab2fbe4892f7c584c
Pravin Bhosale <pb6884845@gmail.com>
2024-02-11 13:38:39 +0530

    create navigation bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,21 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './components/Home.jsx';
+import About from './pages/About.jsx';
+import SignIn from './pages/SignIn.jsx';
+import SignUp from './pages/SignUp.jsx';
+import Shop from './pages/Shop.jsx';
+import NavBar from './components/NavBar.jsx';
 function App() {
-  return <div >hello</div>;
+  return <BrowserRouter>
+    <NavBar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/signin' element={<SignIn/>}/>
+      <Route path='/signup' element={<SignUp/>}/>
+      <Route path='/shop' element={<Shop/>}/>
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App;
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from './NavBar.jsx'
+
+export default function Home() {
+  return (
+    <div>
+       
+    </div>
+  )
+}
